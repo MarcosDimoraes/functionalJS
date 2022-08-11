2e755ba36d448bcf5316a6afc8169c5d382c91e3
Marcos Dimoraes <dimoraes@umbudigital.com>
2022-08-11 09:18:33 -0300

    Update desafio1b.js

--- desafio1b.js
+++ desafio1b.js
@@ -2,12 +2,10 @@ const range = (a, b, c) => {
     const n1 = (b === undefined) ? 1 : a;
     const n2 = (b === undefined) ? a : b;
     const step = (c === undefined) ? ((n1 <= n2) ? 1 : -1) : ((n1 <= n2) ? Math.abs(c) : - Math.abs(c));
-    console.log(n1, n2, step)
     const result = [];
     for (let i = n1; (step > 0) ? i <= n2 : i >= n2; i += step) {
         result.push(i);
     }
-
     return result;
 }
 console.log(range(5));
